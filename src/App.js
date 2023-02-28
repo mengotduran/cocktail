d27dd3107f0e1129667c91b1c66be0d44ee2e967
mengotduran <65475728+mengotduran@users.noreply.github.com>
2023-02-28 22:19:45 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route exact path="/cocktail/:id" element={<SingleCocktail />}>
+            </Route>
+            <Route exact path="*" element={<Error />}>
+            </Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
